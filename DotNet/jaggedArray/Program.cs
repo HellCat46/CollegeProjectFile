0884138c31ee26fa68356ad74f30de9a71bef7fd
HellCat46 <hellcat2388@gmail.com>
2023-12-25 18:06:18 +0530

    Updates JaggedArray

--- DotNet/jaggedArray/Program.cs
+++ DotNet/jaggedArray/Program.cs
@@ -3,3 +3,13 @@
 arr[0] = new[] { 1, 2, 3, 4, 5 };
 arr[1] = new []{1};
 arr[2] = new []{ 3, 4, 2, 1, 5, 3 };
+
+for (int i =0; i<arr.Length; i++)
+{
+    Console.Write("{0}th Row: ", i+1);
+    foreach(int ele in arr[i])
+    {
+        Console.Write(ele+",");
+    }
+    Console.WriteLine();
+}

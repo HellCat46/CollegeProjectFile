05c28b0277b6f2017e5bcce3d620e2dbe5c86994
HellCat46 <hellcat2388@gmail.com>
2024-01-21 12:11:11 +0530

    .

--- Java/Matrix.java
+++ Java/Matrix.java
@@ -5,23 +5,10 @@ public class MatrixMultiplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Rows Count in Matrix 1:");
-        int rowCount1 = sc.nextInt();
-        System.out.print("Enter Rows Count in Matrix 1:");
-        int colCount1 = sc.nextInt();
-        System.out.print("Enter Rows Count in Matrix 2:");
-        int rowCount2 = sc.nextInt();
-        System.out.print("Enter Rows Count in Matrix 2:");
-        int colCount2 = sc.nextInt();
 
-        if(colCount1 != rowCount2) {
-            System.out.println("Matrix multiplication is not possible.");
-            return;
-        }
-
-        int[][] matrix1 = new int[rowCount1][colCount1];
-        for(int idx = 0; idx < rowCount1; idx++){
-            for(int iidx = 0; iidx < colCount1; iidx++){
+        int[][] matrix1 = new int[2][2];
+        for(int idx = 0; idx < 2; idx++){
+            for(int iidx = 0; iidx < 2; iidx++){
                 System.out.printf("Enter Value of Row %d, Column %d: ", idx+1, iidx+1);
                 matrix1[idx][iidx] = sc.nextInt();
             }
@@ -30,22 +17,22 @@ public class MatrixMultiplication {
 
         System.out.println("\n\n");
 
-        int[][] matrix2 = new int[rowCount2][colCount2];
-        for(int idx = 0; idx < rowCount2; idx++){
-            for(int iidx = 0; iidx < colCount2; iidx++){
+        int[][] matrix2 = new int[2][2];
+        for(int idx = 0; idx < 2; idx++){
+            for(int iidx = 0; iidx < 2; iidx++){
                 System.out.printf("Enter Value of Row %d, Column %d: ", idx+1, iidx+1);
                 matrix2[idx][iidx] = sc.nextInt();
             }
             System.out.println();
         }
 
-        int[][] answer = new int[rowCount1][colCount2];
-        for (int idx = 0; idx < rowCount1; idx++)
+        int[][] answer = new int[2][2];
+        for (int idx = 0; idx < 2; idx++)
         {
-            for (int iidx = 0; iidx < colCount2 ; iidx++)
+            for (int iidx = 0; iidx < 2 ; iidx++)
             {
                 int tot = 0;
-                for (int k = 0; k < rowCount2; k++)
+                for (int k = 0; k < 2; k++)
                 {
                     tot += (matrix1[idx][k]*matrix2[k][iidx]);
                 }
---
+++ Java/Sum.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class SumOfNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Amount of Number you want to add: ");
+        int count = sc.nextInt();
+
+        int tot = 0;
+        for(int idx = 0; idx < count; idx++){
+            System.out.printf("Enter %d'th number: ", idx+1);
+            tot += sc.nextInt();
+        }
+        sc.close();
+
+        System.out.println("Total of All Numbers: "+tot);
+    }
+}
